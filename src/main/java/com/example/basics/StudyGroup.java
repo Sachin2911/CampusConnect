d9027e0a0333a40846be77fc2025bb8974dbb9d1
DanielDanzo <140892269+DanielDanzo@users.noreply.github.com>
2023-07-29 23:59:16 +0000

    Add files via upload

---
+++ src/main/java/com/example/basics/StudyGroup.java
@@ -0,0 +1,20 @@
+package com.example.basics;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class StudyGroup extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_study_group, container, false);
+    }
+}
